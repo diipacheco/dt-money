91fbc26d3fe40db34853d05ce9cbdc801e52c66a
diipacheco <edilsonpacheco6@gmail.com>
2024-07-03 16:33:29 -0300

    Adds Summary Component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import { useTheme } from "styled-components";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+
+import { Container, SummaryCard } from "./styles";
+
+export function Summary() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color={theme["green-300"]} />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color={theme["red-300"]} />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color={theme["white"]} />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </Container>
+  );
+}
