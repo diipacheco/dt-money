1642867ab1969afc46abb36db1fc9addd0752e75
diipacheco <edilsonpacheco6@gmail.com>
2024-07-03 16:00:10 -0300

    Configure global styles with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 export function App() {
-  return <h1>hello world</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>hello world</h1>
+    </ThemeProvider>
+  );
 }
